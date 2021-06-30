b855d740067eb8de8e2fd23503c3db42f347397a
m.mercier <mmercier@pc-m.mercier>
2021-06-30 16:49:05 +0200

    version clean

---
+++ src/components/SideBarLeft.vue
@@ -0,0 +1,181 @@
+<template>
+  <!-- le composant est alimenté par  -->
+  <div>
+    <nav
+      id="cmp-nav-left"
+      @mouseover="openNavbar()"
+      @mouseleave="closeNavbar()"
+      :class="navStyle"
+    >
+      <!-- Element central du menu -->
+      <ul class="nav-elements">
+        <!-- elements -->
+        <li
+          v-for="(menu, index) in navElements"
+          :key="'middle' + index"
+          class="elements"
+          :class="
+            menu.topElement
+              ? 'firstElement'
+              : '' || menu.bottomElement
+              ? 'lastElement'
+              : ''
+          "
+        >
+          <!-- logique avec des sous-elements -->
+          <div v-if="menu.childElement">
+              <div
+                :id="'heading' + menu.id"
+                class="pt-3 pb-3 collapsed d-flex align-items-center "
+                type="button"
+                data-bs-toggle="collapse"
+                :data-bs-target="'#' + menu.id"
+                aria-expanded="false"
+                :aria-controls="menu.id"
+              >
+                <a class="myA">
+                  <font-awesome-icon :icon="menu.icon" class="nav-icon" />
+                  {{ menu.name }}
+                  </a
+                >
+                 <span class="accordion-state">&#9658;</span>
+              </div>
+              <ul
+                :id="menu.id"
+                class="collapse"
+                :aria-labelledby="'heading' + menu.id"
+                :data-bs-parent="menu.dataBsParent"
+              >
+                <!-- Sous-menus -->
+                <li
+                  class="sousmenu"
+                  v-for="(sousMenu, index) in menu.sousMenu"
+                  :key="'sub1' + index"
+                  @click="closeNavbar()"
+                >
+                  <router-link
+                    :to="sousMenu.subPath"
+                    class="myA"
+                  >
+                    <font-awesome-icon
+                      :icon="sousMenu.subIcon"
+                      class="nav-submenu-icon"
+                    />
+                    {{ sousMenu.subName }}
+                  </router-link>
+                </li>
+              </ul>
+          </div>
+          <!-- logique sans sous-elements -->
+          <div v-if="!menu.childElement" @click="closeNavbar()">
+              <router-link
+                :to="menu.path"
+                class="pt-3 pb-3 d-flex align-items-center myA"
+                :id="menu.id"
+              >
+                <font-awesome-icon :icon="menu.icon" class="nav-icon" />
+                {{ menu.name }}
+              </router-link>
+          </div>
+          <!-- barre sous le premier élément -->
+          <hr v-if="index === 0" class="top-hr" />
+        </li>
+      </ul>
+    </nav>
+
+    <!-- buttons pour screenTouch -->
+    <div>
+      <!-- Button Wrap -->
+      <div class="toggle-btn" :class="toggled ? 'btn-is-close' : 'btn-is-open'" @click="toggled ? openNavbar() : closeNavbar()">
+        <font-awesome-icon
+          :icon="buttonIcon"
+          aria-hidden="true"
+          class="toggle-btn-icon"
+        />
+      </div>
+
+      <!-- lower btn vient se placer derriere le menu afin de creer un clic de fermeture dans la fenetre. activé uniquement sur les ecrans tactiles  -->
+      <div :class="toggled ? '' : 'lowerBtn'" @click="closeNavbar()"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { Menu } from '@/components/interfaceMenu'
+
+const foo: Menu = {
+  name: 'FOO',
+  id: 'FOO',
+  icon: 'times',
+  dataBsParent: '#cmp-nav-left',
+  topElement: true,
+  childElement: true,
+  sousMenu: [
+    {
+      subName: 'Sous-Menu',
+      subIcon: 'times',
+      subPath: '/bar'
+    },
+    {
+      subName: 'Sous-menu',
+      subIcon: 'times',
+      subPath: '/foo'
+    }
+  ]
+}
+
+const bar: Menu = {
+  name: 'Menu 2',
+  id: 'menu2',
+  icon: 'times',
+  dataBsParent: '#cmp-nav-left',
+  bottomElement: true,
+  childElement: false,
+  path: '/bar2'
+}
+
+@Component({
+  computed: {
+  }
+})
+export default class SideBarLeft extends Vue {
+  navElements: Menu[] = [
+    foo,
+    bar
+  ]
+
+  /* état de la barre
+    navStyle change la classe 'is-close'/'is-open'
+  */
+
+  toggled = true;
+  navStyle = 'is-close'; // change la classe du menu (is-close / is-open)
+
+  /* gestion de l'icone du toggle btn */
+  toggleBtnOpenIt = 'align-justify';
+  toggleBtnCloseBtn = 'times';
+  buttonIcon = this.toggleBtnOpenIt;
+
+  /** controle toggled et change la classe du menu pour fermer la barre */
+  closeNavbar (): void {
+    if (!this.toggled) {
+      this.navStyle = 'is-close'
+      this.toggled = !this.toggled
+      this.buttonIcon = this.toggleBtnOpenIt
+    }
+  }
+
+  /** controle toggled et change la classe du menu pour ouvrir la barre */
+  openNavbar (): void {
+    if (this.toggled) {
+      this.navStyle = 'is-open'
+      this.toggled = !this.toggled
+      this.buttonIcon = this.toggleBtnCloseBtn
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+@import "./src/style";
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap'
+import Vuex from 'vuex'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAlignJustify, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAlignJustify, faTimes)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
